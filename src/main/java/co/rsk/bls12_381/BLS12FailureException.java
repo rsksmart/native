d0ec726cd9f35c1d4322966df88a4a87c93fce6d
fedejinich <federico@iovlabs.org>
2021-02-03 23:01:11 -0300

    BLS12_381 receives data, performs operations internally and throws exception for any error

---
+++ src/main/java/co/rsk/bls12_381/BLS12FailureException.java
@@ -0,0 +1,7 @@
+package co.rsk.bls12_381;
+
+public class BLS12FailureException extends RuntimeException {
+    public BLS12FailureException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/co/rsk/bls12_381/BLS12_381.java
+++ src/main/java/co/rsk/bls12_381/BLS12_381.java
@@ -4,6 +4,9 @@ import co.rsk.NativeLoader;
 import com.sun.jna.Library;
 import com.sun.jna.ptr.IntByReference;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class BLS12_381 implements Library {
     public static final boolean ENABLED;
 
@@ -31,50 +34,58 @@ public class BLS12_381 implements Library {
     public static final byte BLS12_MAP_FP_TO_G1_OPERATION_RAW_VALUE = 8;
     public static final byte BLS12_MAP_FP2_TO_G2_OPERATION_RAW_VALUE = 9;
 
-    public static int g1Add(byte[] data) {
+    public static byte[] g1Add(byte[] data) {
      return performOperation(BLS12_G1ADD_OPERATION_RAW_VALUE, data);
     }
 
-    public static int g1Mul(byte[] data) {
+    public static byte[] g1Mul(byte[] data) {
         return performOperation(BLS12_G1MUL_OPERATION_RAW_VALUE, data);
     }
 
-    public static int g1MultiExp(byte[] data){
+    public static byte[] g1MultiExp(byte[] data){
         return performOperation(BLS12_G1MULTIEXP_OPERATION_RAW_VALUE, data);
     }
 
-    public static int g2Add(byte[] data) {
+    public static byte[] g2Add(byte[] data) {
         return performOperation(BLS12_G2ADD_OPERATION_RAW_VALUE, data);
     }
 
-    public static int g2Mul(byte[] data) {
+    public static byte[] g2Mul(byte[] data) {
         return performOperation(BLS12_G2MUL_OPERATION_RAW_VALUE, data);
     }
 
-    public static int g2MultiExp(byte[] data) {
+    public static byte[] g2MultiExp(byte[] data) {
         return performOperation(BLS12_G2MULTIEXP_OPERATION_RAW_VALUE, data);
     }
 
-    public static int mapFpToG1(byte[] data) {
+    public static byte[] mapFpToG1(byte[] data) {
         return performOperation(BLS12_MAP_FP_TO_G1_OPERATION_RAW_VALUE, data);
     }
 
-    public static int mapFp2ToG2(byte[] data) {
+    public static byte[] mapFp2ToG2(byte[] data) {
         return performOperation(BLS12_MAP_FP2_TO_G2_OPERATION_RAW_VALUE, data);
     }
 
-    public static int pair(byte[] data) {
+    public static byte[] pair(byte[] data) {
         return performOperation(BLS12_PAIR_OPERATION_RAW_VALUE, data);
     }
 
-    private static int performOperation(byte operationId, byte[] data) {
+    private static byte[] performOperation(byte operationId, byte[] data) {
         final byte[] result = new byte[BLS12_381.EIP2537_PREALLOCATE_FOR_RESULT_BYTES];
         final byte[] error = new byte[BLS12_381.EIP2537_PREALLOCATE_FOR_ERROR_BYTES];
 
         final IntByReference o_len = new IntByReference(BLS12_381.EIP2537_PREALLOCATE_FOR_RESULT_BYTES);
         final IntByReference err_len = new IntByReference(BLS12_381.EIP2537_PREALLOCATE_FOR_ERROR_BYTES);
 
-        return BLS12_381.eip2537_perform_operation(operationId, data, data.length, result, o_len, error, err_len);
+        final int errorNo = BLS12_381.eip2537_perform_operation(operationId, data, data.length, result, o_len, error, err_len);
+
+        if (errorNo == 0) {
+            //we need to truncate the result array to the actual length
+            return Arrays.copyOfRange(result, 0, o_len.getValue());
+        } else {
+            String reason = new String(Arrays.copyOfRange(error, 0, err_len.getValue()), StandardCharsets.UTF_8);
+            throw new BLS12FailureException(String.format("precompiled failed: {}", reason));
+        }
     }
 
     private static native int eip2537_perform_operation(
